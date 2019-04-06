492fc49abb538e91eec613c065685764404f6e97
Mees <mees@happyflow.io>
2019-04-06 18:58:48 +0200

    added basic coutry selector

--- extension.js
+++ extension.js
@@ -13,169 +13,183 @@ const Mainloop = imports.mainloop;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
+let defaultCountry = 'netherlands';
+
 const NordVPN = new Lang.Class({
-    Name: "NordVPN status",
-    Extends: PanelMenu.Button,
+	Name: 'NordVPN status',
+	Extends: PanelMenu.Button,
 
-    // function to set custom icons from the 'icons' directory
-    _getCustIcon: function(icon_name) {
-		let gicon = Gio.icon_new_for_string( Me.dir.get_child('icons').get_path() + "/" + icon_name + ".svg" );
+	// function to set custom icons from the 'icons' directory
+	_getCustIcon: function(icon_name) {
+		let gicon = Gio.icon_new_for_string(Me.dir.get_child('icons').get_path() + '/' + icon_name + '.svg');
 		return gicon;
-    },
+	},
 
-    _init: function () {
-        this.parent(0.0, "NordVPN status");
+	_init: function () {
+		this.parent(0.0, 'NordVPN status');
 
-        this.icon = new St.Icon({
-            gicon: this._getCustIcon('nordvpn-changing-symbolic'),
-            style_class: "system-status-icon"
-        });
+		this.icon = new St.Icon({
+			gicon: this._getCustIcon('nordvpn-changing-symbolic'),
+			style_class: 'system-status-icon'
+		});
 
-        let box = new St.BoxLayout({ 
-            vertical: false, 
-            style_class: 'panel-status-menu-box' 
-        });
+		let box = new St.BoxLayout({ 
+			vertical: false, 
+			style_class: 'panel-status-menu-box' 
+		});
 
 		box.add_child(this.icon);
 		this.actor.add_child(box);
 
-        this.inactive_params = {
-            reactive: true,
-            activate: false,
-            hover: false,
-            style_class: null,
-            can_focus: false
-        };
+		this.inactive_params = {
+			reactive: true,
+			activate: false,
+			hover: false,
+			style_class: null,
+			can_focus: false
+		};
 
-    },
+		this.countries = ['Albania', 'Greece', 'Portugal', 'Argentina', 'Hong_Kong', 'Romania', 'Australia', 'Hungary', 'Serbia', 'Austria', 'Iceland', 'Singapore', 'Belgium', 'India', 'Slovakia', 'Bosnia_And_Herzegovina', 'Indonesia', 'Slovenia', 'Brazil', 'Ireland', 'South_Africa', 'Bulgaria', 'Israel', 'South_Korea', 'Canada', 'Italy', 'Spain', 'Chile', 'Japan', 'Sweden', 'Costa_Rica', 'Latvia', 'Switzerland', 'Croatia', 'Luxembourg', 'Taiwan', 'Cyprus', 'Malaysia', 'Thailand', 'Czech_Republic', 'Mexico', 'Turkey', 'Denmark', 'Moldova', 'Ukraine', 'Estonia', 'Netherlands', 'United_Kingdom', 'Finland', 'New_Zealand', 'United_States', 'France', 'North_Macedonia', 'Vietnam', 'Georgia', 'Norway', 'Germany', 'Poland'];
+		
 
-    // refresh the refresh timer and update the connection status
-    _refresh: function () {
-        this._getStatus();
-        this._removeTimeout();
-        this._timeout = Mainloop.timeout_add_seconds(60, Lang.bind(this, this._refresh));
-        return true;
-    },
+	},
 
-    // remove the refresh timer
-    _removeTimeout: function () {
-        if (this._timeout) {
-            Mainloop.source_remove(this._timeout);
-            this._timeout = null;
-        }
-    },
+	// refresh the refresh timer and update the connection status
+	_refresh: function () {
+		this._getStatus();
+		this._removeTimeout();
+		this._timeout = Mainloop.timeout_add_seconds(60, Lang.bind(this, this._refresh));
+		return true;
+	},
 
-    // get the connection status
-    _getStatus: function () {
-        let CMD = ['nordvpn', 'status']
-        this._execCommand(CMD).then(stdout => {
-            this._drawMenu(stdout.split('\n'));
-        });
-    },
+	// remove the refresh timer
+	_removeTimeout: function () {
+		if (this._timeout) {
+			Mainloop.source_remove(this._timeout);
+			this._timeout = null;
+		}
+	},
 
-    // what happens after you've clicked the icon to build the menu
-    _drawMenu: function (status) {
-        this.menu.removeAll();
+	// get the connection status
+	_getStatus: function () {
+		let CMD = ['nordvpn', 'status'];
+		this._execCommand(CMD).then(stdout => {
+			this._drawMenu(stdout.split('\n'));
+		});
+	},
 
-        if (status.length > 5) {
-            // build a menu for when nord vpn is connected
-            this.icon.set_gicon(this._getCustIcon('nordvpn-connected-symbolic'));
-            this.menu.addMenuItem(new PopupMenu.PopupMenuItem(status[4], this.inactive_params));
-            this.menu.addMenuItem(new PopupMenu.PopupMenuItem(status[1], this.inactive_params));
-            this.menu.addMenuItem(new PopupMenu.PopupMenuItem(status[2] + ', ' + status[3], this.inactive_params));
-            this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+	// what happens after you've clicked the icon to build the menu
+	_drawMenu: function (status) {
+		this.menu.removeAll();
 
-            // connection switch
-            this.disconnectItem = new PopupMenu.PopupSwitchMenuItem('connection', true);
-            this.menu.addMenuItem(this.disconnectItem);
+		if (status.length > 5) {
+			// menu for when nordvpn is connected
 
-            this.disconnectItem.connect('toggled', Lang.bind(this, function (object, value) {
-                this.icon.set_gicon(this._getCustIcon('nordvpn-changing-symbolic'));
-                this.disconnectItem.setStatus('closing...');
-                this._disconnect();
-            }));
-            
-        } else {
-            //build a menu for when nordvpn is disconnected
-            this.icon.set_gicon(this._getCustIcon('nordvpn-disconnected-symbolic'));
+			// icon
+			this.icon.set_gicon(this._getCustIcon('nordvpn-connected-symbolic'));
 
-            // connection switch
-            this.connectItemNL = new PopupMenu.PopupSwitchMenuItem('connect (NL)');
-            this.menu.addMenuItem(this.connectItemNL);
-            this.connectItemUS = new PopupMenu.PopupSwitchMenuItem('connect (US)');
-            this.menu.addMenuItem(this.connectItemUS);
+			// stats
+			this.menu.addMenuItem(new PopupMenu.PopupMenuItem(status[4], this.inactive_params));
+			this.menu.addMenuItem(new PopupMenu.PopupMenuItem(status[1], this.inactive_params));
+			this.menu.addMenuItem(new PopupMenu.PopupMenuItem(status[2] + ', ' + status[3], this.inactive_params));
+			this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
-            this.connectItemNL.connect('toggled', Lang.bind(this, function (object, value) {
-                this.icon.set_gicon(this._getCustIcon('nordvpn-changing-symbolic'));
-                this.connectItemNL.setStatus('establishing...');
-                this.connectItemUS.setStatus('ignoring...');
-                this._connect('netherlands');
-            }));
-    
-            this.connectItemUS.connect('toggled', Lang.bind(this, function (object, value) {
-                this.icon.set_gicon(this._getCustIcon('nordvpn-changing-symbolic'));
-                this.connectItemUS.setStatus('establishing...');
-                this.connectItemNL.setStatus('ignoring...');
-                this._connect('united_states');
-            }));
-        }
+			// connection switch
+			this.disconnectItem = new PopupMenu.PopupSwitchMenuItem('connection', true);
+			this.menu.addMenuItem(this.disconnectItem);
 
-    },
+			this.disconnectItem.connect('toggled', Lang.bind(this, (object, value) => {
+				this.icon.set_gicon(this._getCustIcon('nordvpn-changing-symbolic'));
+				this.disconnectItem.setStatus('closing...');
+				this._disconnect();
+			}));
+		} else {
+			// menu for when nordvpn is not connected
 
-    // disconnect nordvpn
-    _disconnect: function () {
-        let CMD = ['nordvpn', 'd'];
-        this._execCommand(CMD).then(() => {
-            this._getStatus();
-        });
-    },
+			// icon
+			this.icon.set_gicon(this._getCustIcon('nordvpn-disconnected-symbolic'));
 
-    // connect to nordvpn
-    _connect: function (country) {
-        let CMD = ['nordvpn', 'c'];
-        CMD.push(country);
-        this._execCommand(CMD).then(() => {
-            this._getStatus();
-        });
-    },
+			// country list submenu
+			// this.countryLabel = new St.Label('connect to: ' + defaultCountry);
+			// this.actor.add_actor(this.countryLabel);
+			let countryMenu = new PopupMenu.PopupSubMenuMenuItem('available countries');
+			for (let i of this.countries) {
+				let countryItem = new PopupMenu.PopupMenuItem(i);
+				countryMenu.menu.addMenuItem(countryItem);
 
-    // execute a command asynchronously
-    // all thanks to this article:
-    // https://github.com/andyholmes/andyholmes.github.io/blob/master/articles/asynchronous-programming-in-gjs.md
-    _execCommand: async function (argv, cancellable=null) {
-        let proc = new Gio.Subprocess({
-            argv: argv,
-            flags: Gio.SubprocessFlags.STDOUT_PIPE
-        });
+				countryItem.connect('activate', Lang.bind(this, (object, value) => {
+					defaultCountry = i;
+				}));
+			}
+			this.menu.addMenuItem(countryMenu);
+			this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
-        proc.init(null);
+			// connection switch
+			this.connectItem = new PopupMenu.PopupSwitchMenuItem('connect');
+			this.menu.addMenuItem(this.connectItem);
 
-        let stdout = await new Promise((resolve, reject) => {
-            proc.communicate_utf8_async(null, cancellable, (proc, res) => {
-                let ok, stdout, stderr;
+			this.connectItem.connect('toggled', Lang.bind(this, (object, value) => {
+				this.icon.set_gicon(this._getCustIcon('nordvpn-changing-symbolic'));
+				this.connectItem.setStatus('establishing...');
+				this._connect(defaultCountry);
+			}));
+		}
+	},
 
-                try {
-                    [ok, stdout, stderr] = proc.communicate_utf8_finish(res);
-                    resolve(stdout);
-                } catch (e) {
-                    reject(e);
-                }
-            });
-        });
+	// disconnect nordvpn
+	_disconnect: function () {
+		let CMD = ['nordvpn', 'd'];
+		this._execCommand(CMD).then(() => {
+			this._getStatus();
+		});
+	},
 
-        return stdout;
-    }
+	// connect to nordvpn
+	_connect: function (country) {
+		let CMD = ['nordvpn', 'c'];
+		CMD.push(country);
+		this._execCommand(CMD).then(() => {
+			this._getStatus();
+		});
+	},
+
+	// execute a command asynchronously
+	// all thanks to this article:
+	// https://github.com/andyholmes/andyholmes.github.io/blob/master/articles/asynchronous-programming-in-gjs.md
+	_execCommand: async function (argv, cancellable=null) {
+		let proc = new Gio.Subprocess({
+			argv: argv,
+			flags: Gio.SubprocessFlags.STDOUT_PIPE
+		});
+
+		proc.init(null);
+
+		let stdout = await new Promise((resolve, reject) => {
+			proc.communicate_utf8_async(null, cancellable, (proc, res) => {
+				let ok, stdout, stderr;
+
+				try {
+					[ok, stdout, stderr] = proc.communicate_utf8_finish(res);
+					resolve(stdout);
+				} catch (e) {
+					reject(e);
+				}
+			});
+		});
+
+		return stdout;
+	}
 
 });
 
 let nordvpn;
 
 function enable() {
-    nordvpn = new NordVPN();
-    nordvpn._refresh();
-    Main.panel.addToStatusArea("nordvpn-status", nordvpn);
+	nordvpn = new NordVPN();
+	nordvpn._refresh();
+	Main.panel.addToStatusArea('nordvpn-status', nordvpn);
 }
 
 function disable() {
-    nordvpn.destroy();
+	nordvpn.destroy();
 }
